8bb6f4d56e2070de7f84a2dca67d103616d05461
MelloBirkan <82703050+MelloBirkan@users.noreply.github.com>
2023-05-09 01:07:17 +0000

    Algumas edições

--- main.py
+++ main.py
@@ -65,7 +65,6 @@ def probabilidade_apos_n_passos(matriz, estado_inicial, n):
   
     # Calcular a matriz de transição de estados elevada a n
     matriz_n = potencia_matriz(matriz, n)
-  
     # Calcular o vetor de probabilidades após n passos
     resultado = [0] * linhas
     for i in range(linhas):
@@ -76,20 +75,8 @@ def probabilidade_apos_n_passos(matriz, estado_inicial, n):
 
 
 # Função para calcular a probabilidade condicional P(X5 = 2, X10 = 3, X15 = 4, X20 = 5 | X0 = 0)
-def probabilidade_condicional(matriz, x5, x10, x15, x20):
-    # Vetor de estado inicial
-    estado_inicial = [0, 0, 0, 0, 0, 1]
-  
-    # Probabilidades de estar em cada estado após 5, 10, 15 e 20 passos a partir do estado inicial
-    p5 = probabilidade_apos_n_passos(matriz, estado_inicial, 5)
-    p10 = probabilidade_apos_n_passos(matriz, estado_inicial, 10)
-    p15 = probabilidade_apos_n_passos(matriz, estado_inicial, 15)
-    p20 = probabilidade_apos_n_passos(matriz, estado_inicial, 20)
-  
-    # Probabilidade condicional P(X5 = 2, X10 = 3, X15 = 4, X20 = 5 | X0 = 0)
-    prob_condicional = p5[0]*matriz[0][1]**2*matriz[1][2]*p10[2]*matriz[2][3]*p15[3]*matriz[3][4]*p20[4]*matriz[4][5]
-  
-    return prob_condicional
+
+
 
 # Calcular a potência da matriz
 expoente = int(input("Digite o expoente da matriz: "))
@@ -115,8 +102,27 @@ def calc_prob_miniaturas():
     
     # calcular a probabilidade usando a matriz de transição
     probabilidade = estado_atual[0] * matriz_transicao[0][1] * matriz_transicao[1][2] * matriz_transicao[2][3] * matriz_transicao[3][4]
-    
+  
+    probabilidade = p5[0]*matriz_transicao[0][1]**2*matriz_transicao[1][2]*p10[2]*matriz_transicao[2][3]*p15[3]*matriz_transicao[3][4]*p20[4]*matriz_transicao[4][5]
+  
     return probabilidade
   
 probabilidade = calc_prob_miniaturas()
-print("\nA probabilidade de P(𝑋5 =2,𝑋10 =3,𝑋15 =4,𝑋20=5|𝑋0 =0) é", probabilidade) 
+print("\nA probabilidade de P(𝑋5 =2,𝑋10 =3,𝑋15 =4,𝑋20=5|𝑋0 =0) é", probabilidade)
+
+
+
+def probabilidade_condicional():
+    # Vetor de estado inicial
+    estado_inicial = [0, 0, 0, 0, 0, 1]
+  
+    # Probabilidades de estar em cada estado após 5, 10, 15 e 20 passos a partir do estado inicial
+    p5 = probabilidade_apos_n_passos(matriz, estado_inicial, 5)
+    p10 = probabilidade_apos_n_passos(matriz, estado_inicial, 10)
+    p15 = probabilidade_apos_n_passos(matriz, estado_inicial, 15)
+    p20 = probabilidade_apos_n_passos(matriz, estado_inicial, 20)
+  
+    # Probabilidade condicional P(X5 = 2, X10 = 3, X15 = 4, X20 = 5 | X0 = 0)
+    prob_condicional = p5[0]*matriz[0][1]**2*matriz[1][2]*p10[2]*matriz[2][3]*p15[3]*matriz[3][4]*p20[4]*matriz[4][5]
+  
+    return prob_condicional
